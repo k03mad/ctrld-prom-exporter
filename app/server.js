e67bcd69984fc5270a15a802feceaaddbd7fc24a
k03mad <k03.mad@gmail.com>
2023-12-20 01:24:28 +0300

    uncomment

--- app/server.js
+++ app/server.js
@@ -17,7 +17,7 @@ if (env.debug) {
 
 app.use(helmet());
 app.use(compression());
-// app.use(express.json());
+app.use(express.json());
 
 app.get('/metrics', async (req, res) => {
     const metrics = await register.metrics();
