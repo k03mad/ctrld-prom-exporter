ae248c6dd38ff5ebdc6bf7461a6ac7a0223705c4
k03mad <k03.mad@gmail.com>
2024-01-18 11:05:47 +0300

    remove unused

--- app/helpers/colors.js
+++ app/helpers/colors.js
@@ -2,5 +2,3 @@ import chalk from 'chalk';
 
 export const errorText = chalk.red.bold.bgGray;
 export const codeText = chalk.gray;
-export const numText = chalk.blue;
-export const nameText = chalk.magenta;
